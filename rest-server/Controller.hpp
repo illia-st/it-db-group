dc83d44c3bcab78054e09ada72d04c37e77ea2fe
Maksym Klymenko <klimenkom14@gmail.com>
2023-11-30 22:41:36 +0200

    delete endpoint

--- rest-server/Controller.hpp
+++ rest-server/Controller.hpp
@@ -75,6 +75,23 @@ public:
     return createResponse(Status::CODE_200, received_message.c_str());
   }
 
+  ENDPOINT("DELETE", "/delete", post, BODY_STRING(oatpp::String, data)) {
+    std::string requestData = data->c_str();
+    std::cout << "Run delete endpoint with data " << requestData << std::endl;
+    m_zmqSocket.send(requestData.data(), requestData.size(), 0);
+
+    zmq::message_t message;
+    const auto ret = m_zmqSocket.recv(message);
+
+    if (!ret.has_value()) {
+        return createResponse(Status::CODE_500, "Error receiving message from server");
+    }
+
+    std::string received_message = std::string(static_cast<char*>(message.data()), message.size());
+
+    return createResponse(Status::CODE_200, received_message.c_str());
+  }
+
 };
 
 #include OATPP_CODEGEN_END(ApiController)
