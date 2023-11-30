a0472cc18466b34bfa8f93cf0bd34e416d3d755b
Maksym Klymenko <klimenkom14@gmail.com>
2023-11-30 20:50:24 +0200

    rest + zmq

--- rest-server/Controller.hpp
+++ rest-server/Controller.hpp
@@ -6,24 +6,61 @@
 #include "oatpp/core/macro/component.hpp"
 #include "oatpp/web/protocol/http/Http.hpp"  
 
+#include <iostream>
+// Include ZeroMQ headers
+#include <zmq.hpp>
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
 class Controller : public oatpp::web::server::api::ApiController {
+private:
+  zmq::context_t m_zmqContext;
+  zmq::socket_t m_zmqSocket;
+
 public:
   Controller(const std::shared_ptr<ObjectMapper>& objectMapper)
-    : oatpp::web::server::api::ApiController(objectMapper)
-  {}
+    : oatpp::web::server::api::ApiController(objectMapper),
+      m_zmqContext(1),
+      m_zmqSocket(m_zmqContext, ZMQ_REQ)
+  {
+    m_zmqSocket.connect("tcp://127.0.0.1:4044");
+    std::cout << "Zmq client connected to the server\n";
+  }
 
 public:
 
   ENDPOINT("GET", "/get", get, QUERY(String, data)) {
-    return createResponse(Status::CODE_200, "Hello from GET!");
+    std::string requestData = data->c_str();
+    std::cout << "Run get endpoint with data " << requestData << std::endl;
+    m_zmqSocket.send(requestData.data(), requestData.size(), 0);
+
+    zmq::message_t message;
+    const auto ret = m_zmqSocket.recv(message);
+
+    if (!ret.has_value()) {
+        return createResponse(Status::CODE_500, "Error receiving message from server");
+    }
+
+    std::string received_message = std::string(static_cast<char*>(message.data()), message.size());
+
+    return createResponse(Status::CODE_200, received_message.c_str());
   }
 
   ENDPOINT("POST", "/post", post, BODY_STRING(String, data)) {
-    
-    return createResponse(Status::CODE_200, "Hello form POST");
+    std::string requestData = data->c_str();
+    std::cout << "Run post endpoint with data " << requestData << std::endl;
+    m_zmqSocket.send(requestData.data(), requestData.size(), 0);
+
+    zmq::message_t message;
+    const auto ret = m_zmqSocket.recv(message);
+
+    if (!ret.has_value()) {
+        return createResponse(Status::CODE_500, "Error receiving message from server");
+    }
+
+    std::string received_message = std::string(static_cast<char*>(message.data()), message.size());
+
+    return createResponse(Status::CODE_200, received_message.c_str());
   }
 
 };
--- rest-server/Server.cpp
+++ rest-server/Server.cpp
@@ -18,21 +18,16 @@ Server::~Server() {
 void Server::run() {
     auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
 
-    // Create and configure ConnectionProvider
     auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({ "localhost", 8000, oatpp::network::Address::IP_4 });
 
-    // Create HttpRouter and add your Controller
     auto router = oatpp::web::server::HttpRouter::createShared();
     auto controller = std::make_shared<Controller>(objectMapper);
     router->addController(controller);
 
-    // Create HttpConnectionHandler with the configured router
     auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
 
-    // Create and configure the server
     oatpp::network::Server server(connectionProvider, connectionHandler);
 
-    // Print information that the server is running
     OATPP_LOGI("Server", "Running on port %s...", connectionProvider->getProperty("port").toString()->c_str());
 
     // Run the server
